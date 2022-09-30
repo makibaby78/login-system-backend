dee0ea43e2c285f5267ce8ee5e59ab40e4aec589
Macky Baby <raindrops.dark@gmail.com>
2022-09-30 22:34:31 +0800

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 
 mongoose.connect(process.env.DATABASE_URL , 
@@ -11,6 +12,7 @@ mongoose.connect(process.env.DATABASE_URL ,
 )
 
 app.use(express.json())
+app.use(cors());
 
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
